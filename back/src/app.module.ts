886bc4a6229595082bb543453dbf78a35fe0aa08
Nikolay Polyakov <misterkola337@gmail.com>
2025-07-15 06:44:44 +0300

    feat: add Prisma module and service for database integration, configure session management with Redis, and enhance environment configuration

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { IS_DEV_ENV } from './libs/common/utils/is-dev.util'
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		}),
+		PrismaModule
+	]
 })
 export class AppModule {}
