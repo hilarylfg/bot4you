62abc8adf8a5b496910c02912bd82bc103f1ca86
hilarylfg <misterkola337@gmail.com>
2025-12-20 02:05:01 +0300

    refactor: remove webpack configuration for fallback and externals in next.config.ts

--- front/next.config.ts
+++ front/next.config.ts
@@ -4,19 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const nextConfig: NextConfig = {
 	poweredByHeader: false,
 	compress: true,
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback = {
-				...config.resolve.fallback,
-				fs: false,
-				net: false,
-				tls: false
-			}
-		}
-		config.externals = [...(config.externals || []), { canvas: 'canvas' }]
-		return config
-	},
 	experimental: {
+		turbopackFileSystemCacheForBuild: false,
+		turbopackFileSystemCacheForDev: false,
 		optimizePackageImports: ['lucide-react', '@radix-ui/react-icons']
 	}
 }
