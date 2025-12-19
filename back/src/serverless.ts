14de1369e7e12c3948ded2332af13316f57c9aa4
hilarylfg <misterkola337@gmail.com>
2025-12-19 14:16:02 +0300

    feat: add serverless support with Redis session management and update Vercel configuration

---
+++ back/src/serverless.ts
@@ -0,0 +1,83 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import RedisStore from 'connect-redis'
+import * as cookieParserImport from 'cookie-parser'
+import express from 'express'
+import * as sessionImport from 'express-session'
+import IORedis from 'ioredis'
+
+import { ms, StringValue } from '@/libs/common/utils/ms.util'
+import { parseBoolean } from '@/libs/common/utils/parse-boolean.util'
+
+import { AppModule } from './app.module'
+
+const cookieParser = cookieParserImport as any
+const session = sessionImport as any
+
+const expressApp = express()
+let cachedApp: any = null
+
+async function bootstrapServer() {
+	if (cachedApp) {
+		return cachedApp
+	}
+
+	const app = await NestFactory.create(
+		AppModule,
+		new ExpressAdapter(expressApp)
+	)
+
+	const config = app.get(ConfigService)
+	const redis = new IORedis(config.getOrThrow('REDIS_URI'))
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true
+		})
+	)
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+				httpOnly: parseBoolean(
+					config.getOrThrow<string>('SESSION_HTTP_ONLY')
+				),
+				secure: parseBoolean(
+					config.getOrThrow<string>('SESSION_SECURE')
+				),
+				sameSite: 'lax'
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER')
+			})
+		})
+	)
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGINS'),
+		credentials: true,
+		exposedHeaders: ['set-cookie']
+	})
+
+	await app.init()
+
+	cachedApp = expressApp
+	return expressApp
+}
+
+export default async (req: any, res: any): Promise<any> => {
+	const app = await bootstrapServer()
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+	return app(req, res)
+}
