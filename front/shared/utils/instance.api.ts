0fb131ef405ff0f0958a6d4903e5bbdcadf2391f
hilarylfg <misterkola337@gmail.com>
2025-12-23 02:55:25 +0300

    refactor: update API base URL and add rewrites in vercel.json for backend integration

--- front/shared/utils/instance.api.ts
+++ front/shared/utils/instance.api.ts
@@ -1,7 +1,7 @@
 import { FetchClient } from './index'
 
 export const api = new FetchClient({
-	baseUrl: process.env.NEXT_PUBLIC_SERVER_URL as string,
+	baseUrl: '/api',
 	options: {
 		credentials: 'include'
 	}
