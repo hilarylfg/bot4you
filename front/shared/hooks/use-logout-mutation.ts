42ec3554e0c8565031053f814ac096dfefccc71c
hilarylfg <misterkola337@gmail.com>
2025-12-23 03:47:13 +0300

    refactor: add SESSION_SAME_SITE configuration and update logout mutation to redirect after successful logout

--- front/shared/hooks/use-logout-mutation.ts
+++ front/shared/hooks/use-logout-mutation.ts
@@ -1,15 +1,19 @@
 import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 import { authService } from '@/shared/services'
 import { toastMessageHandler } from '@/shared/utils'
 
 export function useLogoutMutation() {
+	const router = useRouter()
+
 	const { mutate: logout, isPending: isLoadingLogout } = useMutation({
 		mutationKey: ['logout'],
 		mutationFn: () => authService.logout(),
 		onSuccess() {
 			toast.success('Вы успешно вышли из системы')
+			router.push('/')
 		},
 		onError(error) {
 			toastMessageHandler(error)
